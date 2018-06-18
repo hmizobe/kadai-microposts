1b23c5c687d53d2c3cd82c5164f9375d7e0bc2f1
hiroya.mizobe <hiroya.mizobe@rakuten.com>
2018-06-18 05:57:01 +0000

    timeline

--- app/User.php
+++ app/User.php
@@ -18,9 +18,14 @@ class User extends Authenticatable
         'name', 'email', 'password',
     ];
     
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
+    
+    
     public function microposts() 
     {
-    return $this->hasMany(Micropost::class);
+        return $this->hasMany(Micropost::class);
     }
 
     /**
@@ -28,15 +33,10 @@ class User extends Authenticatable
      *
      * @var array
      */
-    protected $hidden = [
-        'password', 'remember_token',
-    ];
-    
     
     public function followings()
     {
         return $this->belongsToMany(User::class, 'user_follow', 'user_id', 'follow_id')->withTimestamps();
-        
     }
     
     public function followers()
@@ -73,6 +73,13 @@ class User extends Authenticatable
     
     public function is_following($userId) {
         return $this->followings()->where('follow_id', $userId)->exists();
+    }
+    
+    public function feed_microposts()
+    {
+        $follow_user_ids = $this->followings()-> pluck('users.id')->toArray();
+        $follow_user_ids[] = $this->id;
+        return  Micropost::whereIn('user_id', $follow_user_ids);
     }
 
     
